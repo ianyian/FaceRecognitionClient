f9edec9ffb5144d4a9f6775b53782ca1282e6049
XJ <ianyian@gmail.com>
2025-11-28 14:20:33 +0800

    Initial Commit

---
+++ FaceRecognitionClient/FaceRecognitionClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionClientApp.swift
+//  FaceRecognitionClient
+//
+//  Created by XJ on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FaceRecognitionClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
